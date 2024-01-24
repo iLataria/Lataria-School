05dc4555d004f7a1ef1fa490584188dad10bbca5
ilia_lataria <lataria.dev@gmail.com>
2024-01-24 17:38:47 +0400

    Dividing simple exceptions

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace Exceptions
 {
-    internal class Program
+    public class Program
     {
+        public static string DivideByZeroExceptionMsg = "You are trying to divide by zero";
+        public static string ZeroDivideExceptionMsg = "You are trying to divide zero";
+        
         static void Main(string[] args)
         {
+            float result = DivideTwoNumbers(5,0);
+            Console.WriteLine(result);
+        }
+
+        public static float DivideTwoNumbers(float x, float y)
+        {
+            if (y == 0) throw new DivideByZeroException(DivideByZeroExceptionMsg);
+            else if(x == 0) throw new Exception(ZeroDivideExceptionMsg);
+            return x / y;
         }
     }
 }
